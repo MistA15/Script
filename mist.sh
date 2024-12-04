3e656488d0c5cc2623b2ba5273ec6235575a7718
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2024-12-05 04:02:09 +0530

    Update mist.sh

--- mist.sh
+++ mist.sh
@@ -11,7 +11,7 @@ rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
-repo init -u https://github.com/ProjectBlaze-Reborn/manifest.git -b 15 --git-lfs --depth=1
+repo init -u https://github.com/Project-Mist-OS/manifest -b 15 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
@@ -20,24 +20,24 @@ repo init -u https://github.com/ProjectBlaze-Reborn/manifest.git -b 15 --git-lfs
 # rm -rf frameworks/native
 
 # cloning device tree
-git clone https://github.com/BlazeA15/device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
-git clone https://github.com/BlazeA15/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
+git clone https://github.com/MistA15/device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
+git clone https://github.com/MistA15/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
 
 # cloning kernel tree
-git clone https://github.com/BlazeA15/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+git clone https://github.com/MistA15/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 
 # cloning vendor tree
 git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
 git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 
 # cloning hardware tree
-git clone https://github.com/BlazeA15/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+git clone https://github.com/MistA15/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
 # Set up the build environment
 . build/envsetup.sh
 
 # Choose the target device
-lunch blaze_mojito-ap3a-userdebug
+mistify mojito user
 
 # Build the ROM (use mka bacon for a full build)
-mka bacon
+mist b
